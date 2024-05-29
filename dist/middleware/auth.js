1a87d789ec5b791e5f12b6d31a0297a904fc1623
nikhil991gupta <nikhil991gupta@gmail.com>
2024-05-29 18:50:13 +0530

    first commit

---
+++ dist/middleware/auth.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const verifyToken = (req, res, next) => {
+    //console.log('Verify token....................',req);
+    const token = req.cookies["auth_token"] || process.env.DEFAULT_TOKEN;
+    try {
+        //console.log('token',token);
+        const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET_KEY);
+        req.userId = decoded.userId;
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({ message: "unauthorized" });
+    }
+};
+exports.default = verifyToken;
